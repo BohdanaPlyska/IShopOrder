2757f7f348a77235a9861a4cde140e273aa95fdb
Bohanda Plyska <bohdana.plyska@gmail.com>
2023-09-28 11:32:04 +0300

    Initial commit

---
+++ src/main/java/com/example/ishoporder/IShopOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.ishoporder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IShopOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IShopOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ishoporder/IShopOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ishoporder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IShopOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
